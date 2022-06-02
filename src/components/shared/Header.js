b1d0cc8f2a83e35f3081d89f43bb391580463d7c
Ekram Hosen <ekramhosen329@gmail.com>
2022-06-02 22:02:39 +0600

    header done

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <div>
-      <h2>header</h2>
+      <Navbar className="shadow-sm" bg="light" sticky="top" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img height="50" src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/service">
+                Service
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blog">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
